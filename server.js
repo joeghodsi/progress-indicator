9e84e5e5d7130bcfd07ac98dd10c3d93d984bfa5
joeghodsi <joeghodsi@gmail.com>
2014-07-22 15:52:59 -0700

    node server up and running

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-// web.js
-var express = require("express");
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', function(req, res) {
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(logger('dev'));                         // log every request to the console
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({                 // pull information from html in POST
+  extended: true
+}));
+
+//api
+
+app.get('/api/test', function(req, res) {
   res.send('Hello World!');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+//single file view of the app
+
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html');
 });
+
+app.listen(app.get('port'));
+console.log("App listening on port " + apt.get('port'));
