ff1a6dc90fd5e76c7a5356dcabbc7cd5855e2bbf
joeghodsi <joeghodsi@gmail.com>
2014-07-22 17:17:30 -0700

    adding bower files, changing directory structure

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/app'));    // set the static files location /app/foo will be /foo for users
 app.use(logger('dev'));                         // log every request to the console
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({                 // pull information from html in POST
@@ -20,7 +20,7 @@ app.get('/api/test', function(req, res) {
 //application - single file view of the app
 
 app.get('*', function(req, res) {
-  res.sendfile('./public/index.html');
+  res.sendfile('./app/index.html');
 });
 
 app.listen(app.get('port'));
