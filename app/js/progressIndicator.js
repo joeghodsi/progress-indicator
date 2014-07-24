e24a9290c96fe09909a40ff97542ee1690b4e4c9
joeghodsi <joeghodsi@gmail.com>
2014-07-23 19:13:32 -0700

    basic animated bars working. moving on to circular graph next

--- app/js/progressIndicator.js
+++ app/js/progressIndicator.js
@@ -1,70 +1,96 @@
 'use strict';
 
-var app = angular.module('progressIndicatorApp', []);
+var app = angular.module('progressIndicatorApp', ['d3']);
 
 //TODO: determine whether to keep controller or remove. If keeping it, look up more about it.
 
 app.controller('progressController', function($scope) {
-    $scope.actual = 0.73;
-    $scope.expected = 0.5;
+    $scope.title = "Progress Indictator";
+    $scope.d3Data = {
+        actual: 0.73,
+        expected: 0.5
+    };
+    // $scope.actual = 0.73;
+    // $scope.expected = 0.5;
 });
 
-app.directive('ngProgressIndicator', function() {
+app.directive('ngProgressIndicator', ['d3Service', function(d3Service) {
   return {
     restrict: 'AE',
     scope: {
-      actual: '=',
-      expected: '='
+      data: '='
+      // actual: '=',
+      // expected: '='
     },
-    template: '<div class="progressIndicator"><h4>actual: {{ actual }}. expected: {{ expected }}</h4><div class="graph"></div></div>',
+    //template: '<div class="progressIndicator"><h4>actual: {{ data.actual }}.expected: {{ data.expected }}</h4><div class="graph"></div></div>',
     link: function(scope, iElement, iAttrs) {
-      var data = [scope.actual, scope.expected];
-      var x = d3.scale.linear()
-                .domain([0, d3.max(data)])
-                .range([0, 420]);
-      var svg = d3.select(".graph")
-                  .append("div")
-                  .style("width", function(d) { return x(d) + "px"; });
-      // scope.$watchCollection([actual, expected], function(newValues, oldValues, scope) {
-      scope.$watch('actual', function(newVals) {
-        scope.render([scope.actual, scope.expected]);
-      }, true);
+      d3Service.d3().then(function(d3) {
+        // var data = [scope.data.actual, scope.data.expected];
+        var svg = d3.select(iElement[0])
+                    .append('svg')
+                    .style("width", '100%');
 
-      scope.$watch('expected', function(newVals) {
-        scope.render([scope.actual, scope.expected]);
-      }, true);
+        // scope.$watchCollection([actual, expected], function(newValues, oldValues, scope) {
+        scope.$watch('data', function(newVals, oldVals) {
+          scope.renderBars(newVals);
+        }, true);
 
-      scope.render = function(data) {
-        //svg.selectAll('*').remove();
+        // scope.$watch('expected', function(newVals) {
+        //   scope.render([scope.actual, scope.expected]);
+        // }, true);
 
-        //if (!data) return;
+        scope.renderBars = function(data) {
+          svg.selectAll('*').remove();
 
-        var x = d3.scale.linear()
-                  .domain([0, d3.max(data)])
-                  .range([0, 420]);
+          var height = 2 * 35; // 35 = 30(bar height) + 5(margin between bars)
+          svg.attr('height', height);
 
-        svg.select(".graph")
-           .selectAll("div")
-             .data(data)
-           .enter().append("div")
-             .style("width", function(d) { return x(d) + "px"; })
-             .text(function(d) { return d; });
-      }
+          var x = d3.scale.linear()
+                    .domain([0, d3.max(d3.values(data))])
+                    .range([0, 420]);
+
+          svg.selectAll("rect")
+             .data(d3.values(data))
+             .enter()
+               .append("rect")
+               .attr("height", 30) // height of each bar
+               .attr("width", 0) // initial width of 0 for transition
+               .attr("x", 10) // half of the 20 side margin specified above
+               .attr("y", function(d, i){
+                 return i * 35;
+               }) // height + margin between bars
+               .transition()
+                 .duration(1000) // time of duration
+                 .attr("width", function(d){
+                   return x(d) + "px";
+                 }); // width based on scale
+
+
+          svg.selectAll("text")
+             .data(d3.keys(data))
+             .enter()
+               .append("text")
+               .attr("fill", "#fff")
+               .attr("y", function(d, i){return i * 35 + 22;})
+               .attr("x", 15)
+               .text(function(d){return d;});
+        }
+      });
     }
   }
-});
+}]);
 
-var chartGraph = function(actual, expected) {
+// var chartGraph = function(data) {
 
-  var data = [actual, expected];
-  var x = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([0, 420]);
+//   // var data = d3Data;
+//   var x = d3.scale.linear()
+//       .domain([0, d3.max(data)])
+//       .range([0, 420]);
 
-  d3.select(".graph")
-    .selectAll("div")
-      .data(data)
-    .enter().append("div")
-      .style("width", function(d) { return x(d) + "px"; })
-      .text(function(d) { return d; });
-}
+//   d3.select(".graph")
+//     .selectAll("div")
+//       .data(data)
+//     .enter().append("div")
+//       .style("width", function(d) { return x(d) + "px"; })
+//       .text(function(d) { return d; });
+// }
